f628ec16c2465887d916a829fc2db0f2c0918afe
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2026-01-30 09:47:36 +0100

    Implement vehicle data lookup with Streamlit
    Add Streamlit app to fetch vehicle data from API

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+with st.sidebar:
+  st.text_input('skriv inn regnr her')
+
+
+url = f'https://kjoretoyoppslag.atlas.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/{regnr}}'
+data = requests.get(url)
+data = data.json()
+
+last_checked = print(data['periodiskKjoretoykontroll']['sistKontrollert'])
+next_check = print(data['periodiskKjoretoykontroll']['nesteKontroll'])
+
+lastegenskaper = data["tekniskKjoretoy"]["lastegenskaper"]
+
+tilhengervekt_med_brems = lastegenskaper["tillattTilhengervektMedBrems"]
+tilhengervekt_uten_brems = lastegenskaper["tillattTilhengervektUtenBrems"]
+vertikal_koplingslast = lastegenskaper["tillattVertikalKoplingslast"]
+vogntogvekt = lastegenskaper["tillattVogntogvekt"]
+
+modell = data['tekniskKjoretoy']['handelsbetegnelse']
+merke = data['tekniskKjoretoy']['merke']
+
+
+
+st.markdown(f'siste eu-kotnroll: {last_checked}')
+st.markdoown(f'neste eu-kotrnoll: {next_check}')
+
+
+
+
